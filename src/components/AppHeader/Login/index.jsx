0f862aa49cad7ecb791814922b5805b0c08e6bd8
elson <elsonnezaj2005@gmail.com>
2023-05-06 18:09:28 +0200

    created login button to open dialog

--- src/components/AppHeader/Login/index.jsx
+++ src/components/AppHeader/Login/index.jsx
@@ -1,9 +1,32 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Box, Button, Typography } from "@mui/material";
+
+import { handleLoginDialog } from "../../../redux/login/loginSlice";
 
 import styles from "./styles.module.scss";
 
 function Login() {
-  return <Box className={styles.loginContainer}>Login</Box>;
+  const dispatch = useDispatch();
+  const userCredentials = useSelector((state) => state.login.userCredentials);
+
+  return (
+    <Box className={styles.loginContainer}>
+      {userCredentials.username ? (
+        <Typography className={styles.username}>
+          {userCredentials.username}
+        </Typography>
+      ) : (
+        <Button
+          variant="outlined"
+          onClick={() => dispatch(handleLoginDialog(true))}
+          className={styles.loginButton}
+        >
+          LOG IN
+        </Button>
+      )}
+    </Box>
+  );
 }
 export default Login;
