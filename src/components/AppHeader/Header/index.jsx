9f3a03d3da5970296093f14bbb054ed16bfa84f3
elson <elsonnezaj2005@gmail.com>
2023-05-07 21:10:40 +0200

    new font family , new idea for the landing

--- src/components/AppHeader/Header/index.jsx
+++ src/components/AppHeader/Header/index.jsx
@@ -9,12 +9,14 @@ function AppHeader() {
   return (
     <Box className={styles.headerContainer}>
       <Box className={styles.flexHeaderContainer}>
-        <Box className={styles.logoContainer}>ONLINE SECURITY</Box>
-        <Box className={styles.navigationContainer}>
-          <NavigationLinks />
-        </Box>
-        <Box className={styles.userContainer}>
-          <Login />
+        <Box className={styles.logoContainer}>FORSETI PROJECT</Box>
+        <Box className={styles.navContainer}>
+          <Box className={styles.navigationContainer}>
+            <NavigationLinks />
+          </Box>
+          <Box className={styles.userContainer}>
+            <Login />
+          </Box>
         </Box>
       </Box>
     </Box>
--- src/components/MainContainer/index.jsx
+++ src/components/MainContainer/index.jsx
@@ -4,11 +4,13 @@ import { Box } from "@mui/material";
 
 import styles from "./styles.module.scss";
 import AppHeader from "../AppHeader/Header";
+import Landing from "../Landing";
 
 function MainContainer() {
   return (
     <Box className={styles.mainContainer}>
       <AppHeader />
+      <Landing />
     </Box>
   );
 }
